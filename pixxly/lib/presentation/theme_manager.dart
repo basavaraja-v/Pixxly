0e2fae7c0b360287c285b096c56c20ed773fdf62
50-Course <erioluwaadeodu@my.uopeople.edu>
2022-07-23 07:08:52 +0100

    :sparkles: Initialized a theme manager
    Manage app-level themes with theme constants. Temporarily scaffolded with
    :bulb: todo comments.

---
+++ pixxly/lib/presentation/theme_manager.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+ThemeData getApplicationTheme() {
+  return ThemeData(
+      // *: Main app colors
+
+      // *: AppBar theme
+
+      // *: Card view colors
+
+      // *: Text theme
+
+      // *: Input & Decoration themes
+
+      );
+}
